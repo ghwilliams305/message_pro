20f0a22eb29dd39e3e7300b99a5c638ea862881e
George Williams <george@localhost.localdomain>
2023-11-10 12:06:23 -0700

    Adds Word Bank

---
+++ script/message.js
@@ -0,0 +1,10 @@
+//Setting up a word bank
+const verbs = ['am', 'radiate', 'choose', 'trust', 'attract', 'love'];
+const abjectives = ['confident', 'worthy', 'enough', 'postive', 'successful', 'strong', 'limitless', 'resilient', 'attractive'];
+const traits = ['love', 'confidence', 'positive energy', 'gratude', 'harmony', 'peace', 'greatness', 'beauty'];
+const objective = ['my ability', 'my family', 'myself', 'my mind', 'my body', 'my soul'];
+
+console.log(verbs);
+console.log(abjectives);
+console.log(traits);
+console.log(objective);
