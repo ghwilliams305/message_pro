8ef98a9dad7873b4ab6c83463bab1d3807f86d24
George Williams <george@localhost.localdomain>
2023-11-11 11:41:36 -0700

    Adds Random Word Function

--- script/message.js
+++ script/message.js
@@ -2,9 +2,16 @@
 const verbs = ['am', 'radiate', 'choose', 'trust', 'attract', 'love'];
 const abjectives = ['confident', 'worthy', 'enough', 'postive', 'successful', 'strong', 'limitless', 'resilient', 'attractive'];
 const traits = ['love', 'confidence', 'positive energy', 'gratude', 'harmony', 'peace', 'greatness', 'beauty'];
-const objective = ['my ability', 'my family', 'myself', 'my mind', 'my body', 'my soul'];
+const objects = ['my ability', 'my family', 'myself', 'my mind', 'my body', 'my soul'];
 
-console.log(verbs);
-console.log(abjectives);
-console.log(traits);
-console.log(objective);
+//Function that presents a random word from a given array
+const randWord = (array) => array[Math.floor(Math.random() * array.length)];
+
+//testing values of word banks
+console.log(`verbs = ${verbs}`);
+console.log(`abjectives = ${abjectives}`);
+console.log(`traits = ${traits}`);
+console.log(`objects = ${objects}`);
+
+//testing randWord Function
+for(let x = 0; x < 9; x++) console.log(`randWord(verbs) => ${randWord(verbs)}`);
